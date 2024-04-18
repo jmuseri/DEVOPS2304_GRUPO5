ca117bd701fc41372fe0426bdc28686810a3be4e
Ubuntu <ubuntu@ip-172-31-80-87.ec2.internal>
2024-04-18 23:59:30 +0000

    Agrego install_tools.sh con todo lo necesario

---
+++ 00_ec2/install_tools.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+echo "Installing AWS CLI"
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+
+echo "Installing kubectl"
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
+kubectl version --client
+
+echo "Installing eksctl"
+curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+sudo mv /tmp/eksctl /usr/local/bin
+eksctl version
+
+echo "Installing Docker"
+sudo apt update
+sudo apt install -y docker.io
+sudo usermod -aG docker $USER
+
+echo "Installing Docker Compose"
+sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+echo "Installing Helm"
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
+
+echo "Installing Terraform"
+sudo snap install terraform --classic
