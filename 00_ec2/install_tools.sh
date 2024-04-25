06a08a4b1c2bc913fc59bf39673367922a96e85b
Jonathan Museri <45659669+jmuseri@users.noreply.github.com>
2024-04-24 23:26:32 -0300

    Update install_tools.sh

--- 00_ec2/install_tools.sh
+++ 00_ec2/install_tools.sh
@@ -8,27 +8,38 @@ sudo ./aws/install
 echo "Installing kubectl"
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 chmod +x ./kubectl
-sudo mv ./kubectl /usr/local/bin/kubectl
+mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin
+echo 'export PATH=$PATH:$HOME/bin' >> ~/.bashrc
 kubectl version --client
 
-echo "Installing eksctl"
+echo "Installing ekctl"
 curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
 sudo mv /tmp/eksctl /usr/local/bin
+export PATH=$PATH:/usr/local/bin
+echo 'export PATH=$PATH:/usr/local/bin' >> ~/.bashrc
 eksctl version
 
-echo "Installing Docker"
-sudo apt update
-sudo apt install -y docker.io
-sudo usermod -aG docker $USER
-
-echo "Installing Docker Compose"
-sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
+echo "Installing docker"
+sudo apt install -y docker
+sudo usermod -a -G docker ec2-user
+newgrp docker
+wget https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) 
+sudo mv docker-compose-$(uname -s)-$(uname -m) /usr/local/bin/docker-compose
+sudo chmod -v +x /usr/local/bin/docker-compose
+sudo systemctl enable docker.service
+sudo systemctl start docker.service
 
 echo "Installing Helm"
-curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
-chmod 700 get_helm.sh
-./get_helm.sh
+sudo curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+sudo chmod 700 get_helm.sh
+sudo ./get_helm.sh
 
-echo "Installing Terraform"
-sudo snap install terraform --classic
+echo "Installing terraform "
+# Download the installer script:
+sudo curl --proto '=https' --tlsv1.2 -fsSL https://get.opentofu.org/install-opentofu.sh -o install-opentofu.sh
+
+# Give it execution permissions:
+sudo chmod +x install-opentofu.sh
+
+# Run the installer:
+sudo ./install-opentofu.sh --install-method deb
